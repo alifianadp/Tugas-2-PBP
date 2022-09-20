ca571840b3e5df9bfa3085556ac696bd2c695edc
alifianadp <alifianadp@gmail.com>
2022-09-20 22:21:51 +0700

    Data Delivery - Tugas 3

---
+++ mywatchlist/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from mywatchlist.models import MyWatchList
+from django.http import HttpResponse
+from django.core import serializers
+
+# Create your views here.
+def show_mywatchlist(request):
+    data_mywatchlist= MyWatchList.objects.all()
+    context = {
+        'list_watch': data_mywatchlist,
+        'nama': 'Alifia Nadiya Putri',
+        'npm': '2106708002',
+    }
+    return render(request, "mywatchlist.html", context)
+
+def show_xml(request):
+    data = MyWatchList.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = MyWatchList.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
