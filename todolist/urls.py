8d0dbbce4332ca7002aaec5af1e7aa475161f8f8
alifianadp <alifianadp@gmail.com>
2022-10-13 09:34:23 +0700

    Tugas 6 - Javascript dan AJAX

--- todolist/urls.py
+++ todolist/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from todolist.views import show_todolist, register, login_user, logout_user, add_task, change_status, delete_task
+from todolist.views import show_todolist, register, login_user, logout_user, add_task, change_status, delete_task, show_todolist_ajax, show_todolist_json, add_task_ajax
 
 app_name = 'todolist'
 
@@ -10,5 +10,9 @@ urlpatterns = [
     path('logout/', logout_user, name='logout'),
     path('create-task/', add_task, name='add_task'),
     path('change-status/', change_status, name='change_status'),
-    path('delete-task/', delete_task, name='delete_task')
+    path('delete-task/', delete_task, name='delete_task'),
+    # Tugas 6 : AJAX
+    path('json/', show_todolist_json, name='show_todolist_json'),
+    path('ajax/', show_todolist_ajax, name='show_todolist_ajax'),
+    path('add/', add_task_ajax, name='add_task_ajax')
 ]
