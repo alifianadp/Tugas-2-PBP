7851c67dff7b1d288def1a0bca6d6090a0006e4f
alifianadp <alifianadp@gmail.com>
2022-12-03 08:25:35 +0700

    Migrate Railway

--- todolist/views.py
+++ todolist/views.py
@@ -1,10 +1,8 @@
-from django.shortcuts import render
-from django.shortcuts import redirect
+from django.shortcuts import render, redirect
 from todolist.models import Task
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-from django.contrib.auth import authenticate, login
-from django.contrib.auth import logout
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from todolist.forms import TaskForm
 from django.contrib.auth.models import User
